d62606b79206d0898deb9447b5f3eb851617e416
kc900201 <nkc900201@gmail.com>
2021-02-22 14:27:42 +0900

    modify classifier for Pytorch ML training

--- Machine Learning/Pytorch/tutorials/classifier.py
+++ Machine Learning/Pytorch/tutorials/classifier.py
@@ -1,5 +1,14 @@
 # How to train an image classifier
 # Libraries
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt
+from neural_network import Net
+
 
 # Steps
 # 1. Load and normalizing the CIFAR10 training and test datasets using torchvision
@@ -7,3 +16,46 @@
 # 3. Define a loss function
 # 4. Train the network on the training data
 # 5. Test the network on the test data
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testLoader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse')
+
+# functions to show an image
+def imshow(img):
+    img = img / 2 + 0.5 # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+# get random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# Import neural network
+net = Net()
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+# Train network
+for epoch in range(10):
+    running_loss = 0.0
+
+    for i, data in enumerate(trainloader, 0)
+
+
