5c2ea0be26e4d84b156bc33c892bfd6bb1a5ce10
kc900201 <nkc900201@gmail.com>
2021-03-15 13:35:49 +0900

    Begin transfer learning training using Pytorch (3/15/2021)

---
+++ Machine Learning/Pytorch/Transfer Learning/train.py
@@ -0,0 +1,37 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0.0
+
+#  Continue 3/15/2021 midnight
+
