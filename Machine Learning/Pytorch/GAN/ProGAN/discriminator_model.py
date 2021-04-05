e1b32c45f52607376cad491b97a40809e9bcd30f
kc900201 <nkc900201@gmail.com>
2021-04-05 22:59:47 +0900

    Initialize discriminator_model and generator_model for ProGAN

---
+++ Machine Learning/Pytorch/GAN/ProGAN/discriminator_model.py
@@ -0,0 +1,5 @@
+import torch.nn as nn
+
+
+class Discriminator(nn.Module):
+    pass
---
+++ Machine Learning/Pytorch/GAN/ProGAN/generator_model.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+
+factors = [1, 1, 1, 1, 1 / 2, 1 / 4, 1 / 8, 1 / 16, 1 / 32]
+
+
+class WSConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, gain=2):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.scale = (gain / (in_channels * kernel_size ** 2)) ** 0.5
+        self.bias = self.conv.bias
+
+        self.conv.bias = None
+
+        # initialize conv layer
+        nn.init.normal_(self.conv.weight)
+        nn.init.zeros_(self.bias)
+
+    def forward(self, x):
+        return self.conv(x * self.scale) + self.bias.view(1, self.bias.shape[0], 1, 1)
+
+
+class PixelNorm(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.epsilon = 1e-8
+
+    def forward(self, x):
+        return x / torch.sqrt(torch.mean(x ** 2, dim=1, keepdim=True) + self.epsilon)
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, use_pixelnorm=True):
+        super().__init__()
+        self.conv1 = WSConv2d(in_channels, out_channels)
+        self.conv2 = WSConv2d(out_channels, out_channels)
+        self.leaky = nn.LeakyReLU(.2)
+        self.pn = PixelNorm()
+        self.use_pn = use_pixelnorm
+
+    def forward(self, x):
+        x = self.leaky(self.conv1(x))
+        x = self.pn(x) if self.use_pn else x
+        x = self.leaky(self.conv2(x))
+        x = self.pn(x) if self.use_pn else x
+        return x
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, in_channels, img_channels=3):
+        super().__init__()
+        self.initial = nn.Sequential(
+            PixelNorm(),
+            nn.ConvTranspose2d(z_dim, in_channels, 4, 1, 0), # 1 x 1 -> 4 x 4
+            nn.LeakyReLU(0.2),
+            WSConv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.2),
+            PixelNorm()
+        )
+
+    def fade_in(self, alph, upscaled, generated):
+        pass
+
+    def forward(self, x):
+        pass
