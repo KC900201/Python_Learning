b6a70ca8df0b12c4fb7ea2057f21bb67aaacae03
kc900201 <nkc900201@gmail.com>
2021-04-15 13:38:09 +0900

    Basics revision on indexing, math operations and tensor reshaping

--- Machine Learning/Pytorch/tutorials/Pytorch_Tutorials/tensor_math_comparison.py
+++ Machine Learning/Pytorch/tutorials/Pytorch_Tutorials/tensor_math_comparison.py
@@ -8,44 +8,84 @@ def main():
     # Addition
     z = x + y
     z_2 = torch.add(x, y)  # same
-    print(z)
-    print(z_2)
+    # print(z)
+    # print(z_2)
 
     # Subtraction
     z = z - x
-    print(z)
+    # print(z)
 
     # Division
     z = torch.true_divide(x, y)
-    print(z)
+    # print(z)
 
     # inplace operations
     t = torch.zeros(3)
     t.add_(x)
     t += x
-    print(t)
+    # print(t)
 
     # Exponentiation
     z = x.pow(2)
-    print(z)
+    # print(z)
     z = x ** 2
-    print(z)
+    # print(z)
 
     # Matrix multiplication
     x1 = torch.rand((2, 4))
-    x2 = torch.rand((5, 3))
+    x2 = torch.rand((4, 2))
     x3 = torch.mm(x1, x2)
+    # print(x3)
+    # print(x3.shape)
     x3 = x1.mm(x2)
+    # print(x3)
 
     # matrix exponentiation
     matrix_exp = torch.rand(5, 5)
-    print(matrix_exp.matrix_power(3))
+    # print(matrix_exp.matrix_power(3))
 
     # dot product
     z = torch.dot(x, y)
-    print(z)
+    # print(z)
 
     # Batch Matrix Multiplication
+    batch = 32
+    n = 10
+    m = 20
+    p = 30
+
+    tensor1 = torch.rand((batch, n, m))
+    tensor2 = torch.rand((batch, m, p))
+    out_bmm = torch.bmm(tensor1, tensor2)  # (batch, n, p)
+    # print(tensor1)
+    # print(tensor2)
+    # print(out_bmm)
+
+    # Example of Broadcasting
+    x1 = torch.rand((5, 5))
+    x2 = torch.rand((1, 5))
+    print(x1)
+    print(x2)
+
+    z = x1 - x2
+    print(z)
+    z = x1 ** x2
+    print(z)
+
+#     Other useful tensor operations
+    sum_x = torch.sum(x, dim=0)
+    values, indices = torch.max(x, dim=0)
+    values, indices = torch.min(x, dim=0)
+    abs_x = torch.abs(x)
+
+    z = torch.argmax(x, dim=0)
+    z = torch.argmin(x, dim=0)
+
+    mean_x = torch.mean(x.float(), dim=0)
+    z = torch.eq(x, y)
+    sorted_y, indices = torch.sort(y, dim=0, descending=False)
+
+    z = torch.clamp(x, min=0)
 
 
 if __name__ == '__main__':
