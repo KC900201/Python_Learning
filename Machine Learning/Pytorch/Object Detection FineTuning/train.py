0e599de4019ae0f4b5efb0bf0d8583527f3c5d2e
kc900201 <nkc900201@gmail.com>
2021-04-20 22:40:06 +0900

    Complete (halfway) train.py. Need to refer to URL for completion

--- Machine Learning/Pytorch/Object Detection FineTuning/train.py
+++ Machine Learning/Pytorch/Object Detection FineTuning/train.py
@@ -1,10 +1,14 @@
+# Reference URL
+# https://discuss.pytorch.org/t/modulenotfounderror-no-module-named-engine/59564/5
+# https://github.com/pytorch/vision/tree/master/references
+
 import torch.utils.data
 import torchvision
 from engine import train_one_epoch, evaluate
 
 import utils
 from dataset import PennFudanDataset
-from utils import get_transform
+from utils import get_transform, device
 
 
 def train():
@@ -24,6 +28,7 @@ def train():
     x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
     predictions = model(x)  # Returns predictions
 
+
 # Main function for training
 def main():
     num_classes = 2
@@ -37,7 +42,35 @@ def main():
     dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
 
     # define training and validation data loaders
-    data_loader = torch.utils.data.DataLoader(dataset, batch_size=12, shuffle=True, num_workers=4, collate_fn=utils.collate_fn)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=12, shuffle=True, num_workers=4,
+                                              collate_fn=utils.collate_fn)
+
+    data_loader_test = torch.utils.data.DataLoader(dataset_test, batch_size=1, shuffle=False, num_workers=4,
+                                                   collate_fn=utils.collate_fn())
+
+    # get the model using our helper function
+    model = get_model_instance_segmentation(num_classes)
+
+    # move model to the right device
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+
+    # and a learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+    # let's train it for 10 epochs
+    num_epochs = 10
+
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluate(model, data_loader_test, device=device)
 
 
 if __name__ == '__main__':
