49ecb100d25945819878192d6d81751cde2f69b6
kc900201 <nkc900201@gmail.com>
2021-04-24 00:33:15 +0900

    Completed train.py src code. Now completing main.py

---
+++ Machine Learning/Pytorch/tutorials/DQN/main.py
@@ -0,0 +1,6 @@
+# Main training loop
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- Machine Learning/Pytorch/tutorials/DQN/train.py
+++ Machine Learning/Pytorch/tutorials/DQN/train.py
@@ -1,14 +1,12 @@
-import random
 import math
-import dqn_model
-import utils
+import random
+
+import matplotlib.pyplot as plt
 import torch
 import torch.optim as optim
 
-import matplotlib.pyplot as plt
-
-from dqn_model import env, get_screen, DQN, ReplayMemory, is_ipython
-from utils import BATCH_SIZE, GAMMA, EPS_END, EPS_DECAY, EPS_START, TARGET_UPDATE, device
+from dqn_model import env, get_screen, DQN, ReplayMemory, is_ipython, Transition
+from utils import BATCH_SIZE, GAMMA, EPS_END, EPS_DECAY, EPS_START, device
 
 init_screen = get_screen()
 _, _, screen_height, screen_width = init_screen.shape
@@ -68,4 +66,36 @@ def plot_durations():
         display.clear_output(wait=True)
         display.display(plt.gcf())
 
+
 # Training loop
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+
+    batch = Transition(*zip(*transitions))
+
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device,
+                                  dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    loss = F.smooth_11_los(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+
+    for param in policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
