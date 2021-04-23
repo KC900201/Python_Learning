68ac026b7726ea9e95c62f1cdfee0d2e55bfef2b
kc900201 <nkc900201@gmail.com>
2021-04-23 13:46:26 +0900

    Writing training model of DQN (WIP)

--- Machine Learning/Pytorch/tutorials/DQN/dqn_model.py
+++ Machine Learning/Pytorch/tutorials/DQN/dqn_model.py
@@ -4,22 +4,19 @@ from collections import namedtuple
 import gym
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as T
 from PIL import Image
 
-env = gym.make('CartPole-v0').unwrapped
+from utils import device
 
-# if gpu is to be used
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+env = gym.make('CartPole-v0').unwrapped
 
 # set up matplotlib
 is_ipython = 'inline' in matplotlib.get_backend()
-
-if (is_ipython):
-    pass
 
 plt.ion()
 
@@ -88,3 +85,32 @@ def get_cart_location(screen_width):
     world_width = env.x_threshold * 2
     scale = screen_width / world_width
     return int(env.state[0] * scale * screen_width / 2.0)  # MIDDLE OF CART
+
+
+def get_screen():
+    screen = env.render(mode='rgb_array').transpose((2, 0, 1))
+    _, screen_height, screen_width = screen.shape
+    screen = screen[:, int(screen_height * 0.4):int(screen_height * 0.8)]
+    view_width = int(screen_width * 0.6)
+    cart_location = get_cart_location(screen_width)
+
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(cart_location - view_width // 2, cart_location + view_width // 2)
+    # Strip off the edges, so that we have a square image centered on a cart
+    screen = screen[:, :, slice_range]
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    # Resize, and add a batch dimension (BCHW)
+    return resize(screen).unsqueeze(0).to(device)
+
+
+env.reset()
+plt.figure()
+plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(), interpolation='none')
+plt.title('Example extracted screen')
+plt.show()
