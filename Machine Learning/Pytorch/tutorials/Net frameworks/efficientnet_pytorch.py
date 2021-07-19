0e847338544cf977406b4be7cd6e18d50b42fc03
kc900201 <nkc900201@gmail.com>
2021-07-19 13:32:34 +0900

    Begin to code EfficientNet model

--- Machine Learning/Pytorch/tutorials/Net frameworks/efficientnet_pytorch.py
+++ Machine Learning/Pytorch/tutorials/Net frameworks/efficientnet_pytorch.py
@@ -26,15 +26,59 @@ phi_values = {
     "b7": (6, 600, 0.5),
 }
 
+
 # Architecture coding
 class CNNBlock(nn.Module):
-    pass
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, groups=1):
+        super(CNNBlock, self).__init__()
+        self.cnn = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            groups=groups,
+        )
+
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.silu = nn.SiLU()  # ?? SiLU <-> Swish
+
+    def forward(self, x):
+        return self.silu(self.bn(self.cnn(x)))
+
 
 class SqueezeExcitation(nn.Module):
-    pass
+    def __init__(self, in_channels, reduced_dim):
+        super(SqueezeExcitation, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),  # C x H x W -> C x 1 x 1
+            nn.Conv2d(in_channels, reduced_dim, 1),
+            nn.SiLU(),
+            nn.Conv2d(reduced_dim, in_channels, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return x * self.se(x)
+
 
 class InvertedResidualBlock(nn.Module):
-    pass
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride,
+                 padding,
+                 expand_ratio,
+                 reduction=4,  # squeeze excitation
+                 survival_prop=0.8,  # for stochastic depth
+                 ):
+        super(InvertedResidualBlock, self).__init__()
+        self.survival_prob = 0.8
+        self.use_residual = in_channels == out_channels and stride == 1
+        self.hidden_dim = in_channels * expand_ratio
+        # Continue 7/19/2021
+
 
 class EfficientNet(nn.Module):
     pass
