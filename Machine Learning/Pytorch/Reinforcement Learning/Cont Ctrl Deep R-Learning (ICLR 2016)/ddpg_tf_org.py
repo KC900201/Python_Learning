efa5e377363203f543928601f81e65bb6eb69808
kc900201 <nkc900201@gmail.com>
2021-11-11 13:29:06 +0900

    Learning DDPG deep reinforcement learning paper implementation

---
+++ Machine Learning/Pytorch/Reinforcement Learning/Cont Ctrl Deep R-Learning (ICLR 2016)/ddpg_tf_org.py
@@ -0,0 +1,6 @@
+# Youtube reference for paper implementation - https://www.youtube.com/watch?v=GJJc1t0rtSU&ab_channel=freeCodeCamp.org
+
+# Need a replay buffer class
+# Need a class for a  target Q network
+# Use batch-normalization (normalizing inputs)
+# policy is deterministic, how to handle explorer exploit dilemma?
