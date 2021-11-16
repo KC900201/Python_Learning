987f77b59563c9530361cde7159bd4535e5ddc77
kc900201 <nkc900201@gmail.com>
2021-11-16 13:08:57 +0900

    Updating comments for DDPG

--- Machine Learning/Pytorch/Reinforcement Learning/Cont Ctrl Deep R-Learning (ICLR 2016)/ddpg_tf_org.py
+++ Machine Learning/Pytorch/Reinforcement Learning/Cont Ctrl Deep R-Learning (ICLR 2016)/ddpg_tf_org.py
@@ -4,3 +4,9 @@
 # Need a class for a  target Q network
 # Use batch-normalization (normalizing inputs)
 # policy is deterministic, how to handle explorer exploit dilemma?
+# deterministic policy ~ outputs the actual action instead of a probability
+# will need a way to bound the actions to the env limits
+# We have two actor and two critic networks, a target for each.
+# Updates are soft, according to theta_prime = tau*theta + (1-tau)*theta_prime, with tau << 1
+# The target actor is just the evaluation actor plus some noise process
+# they used Ornstein Uhlenbeck (look up later!) -> need a class for the noise
