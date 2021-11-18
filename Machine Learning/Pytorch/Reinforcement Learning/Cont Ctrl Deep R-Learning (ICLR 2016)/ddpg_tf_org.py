dc866e11ef7a39e4dd4278389b5a3ccd200bf109
kc900201 <nkc900201@gmail.com>
2021-11-18 13:33:22 +0900

    Initial transformations coding for ddpg paper reference

--- Machine Learning/Pytorch/Reinforcement Learning/Cont Ctrl Deep R-Learning (ICLR 2016)/ddpg_tf_org.py
+++ Machine Learning/Pytorch/Reinforcement Learning/Cont Ctrl Deep R-Learning (ICLR 2016)/ddpg_tf_org.py
@@ -10,3 +10,64 @@
 # Updates are soft, according to theta_prime = tau*theta + (1-tau)*theta_prime, with tau << 1
 # The target actor is just the evaluation actor plus some noise process
 # they used Ornstein Uhlenbeck (look up later!) -> need a class for the noise
+
+import os
+import numpy as np
+import tensorflow as tf
+
+
+class OUActionNoise(object):
+    def __init__(self, mu, sigma=0.15, theta=0.2, dt=1e-2, x0=None):
+        self.theta = theta
+        self.mu = mu
+        self.dt = dt
+        self.sigma = sigma
+        self.x0 = x0
+        self.reset()
+
+    def __call__(self):
+        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
+            self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
+        self.x_prev = x
+        return x
+
+    def reset(self):
+        self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
+
+
+class ReplayBuffer(object):
+    def __init__(self, max_size, input_shape, n_actions):
+        self.mem_size = max_size
+        self.mem_cntr = 0
+        self.state_memory = np.zeros((self.mem_size, *input_shape))
+        self.new_state_memory = np.zeros((self.mem_size, *input_shape))
+        self.action_memory = np.zeros((self.mem_size, n_actions))
+        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.float32)
+
+    def store_transition(self, state, action, reward, state_, done):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        self.action_memory[index] = action
+        self.terminal_memory[index] = 1 - int(done)
+        self.mem_cntr += 1
+
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.mem_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, batch_size)
+
+        states = self.state_memory[batch]
+        new_states = self.new_state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        terminal = self.terminal_memory[batch]
+
+        return states, actions, rewards, new_states, terminal
+
+
+class Actor(object):
+    def __init__(self, lr, n_actions, name, input_dims, sess, fc1_dims, fc2_dims, action_bound, batch_size=64,
+                 chkpt_dir='tmp/ddpg'):
+        pass
+#     Continue 11/18/2021
