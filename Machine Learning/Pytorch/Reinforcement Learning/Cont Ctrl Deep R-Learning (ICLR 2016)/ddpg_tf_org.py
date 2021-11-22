4e630d7276c6c996f8e75cab7d8a7b1f54d3a926
kc900201 <nkc900201@gmail.com>
2021-11-22 13:23:25 +0900

    WIP building the module for continuous deep reinforcement learning

--- Machine Learning/Pytorch/Reinforcement Learning/Cont Ctrl Deep R-Learning (ICLR 2016)/ddpg_tf_org.py
+++ Machine Learning/Pytorch/Reinforcement Learning/Cont Ctrl Deep R-Learning (ICLR 2016)/ddpg_tf_org.py
@@ -12,8 +12,11 @@
 # they used Ornstein Uhlenbeck (look up later!) -> need a class for the noise
 
 import os
+import random
+
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.initializers import random_uniform
 
 
 class OUActionNoise(object):
@@ -69,5 +72,59 @@ class ReplayBuffer(object):
 class Actor(object):
     def __init__(self, lr, n_actions, name, input_dims, sess, fc1_dims, fc2_dims, action_bound, batch_size=64,
                  chkpt_dir='tmp/ddpg'):
-        pass
-#     Continue 11/18/2021
+        self.lr = lr
+        self.n_actions = n_actions
+        self.name = name
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.sess = sess
+        self.batch_size = batch_size
+        self.action_bound = action_bound
+        self.chkpt_dir = chkpt_dir
+        self.build_network()
+        self.params = tf.Module.trainable_variables(scope=self.name)  # using tensorflow v2 module
+        self.saver = tf.train.Checkpoint()  # migrate to tensorflow v2 (Saver is v1)
+        self.checkpoint_file = os.path.join(chkpt_dir, name + '_ddpg.ckpt')
+        self.action_gradients = list(map(lambda x: tf.divide(x, self.batch_size), self.unnormalized_actor_gradients))
+        self.unnormalized_actor_gradients = tf.gradients(self.mu, self.params, -self.action_gradient)
+        self.optimize = tf.keras.optimizers.Adam(self.lr). \
+            apply_gradients(zip(self.action_gradients, self.params))
+        # tf.compat.v1.trainable_variables
+
+    def build_network(self):
+        with tf.compat.v1.variable_scope(self.name):
+            # self.input = tf.placeholder(tf.float32, shape=[None, self. * input_dims], name='inputs') // wait for fixation
+            self.action_gradient = tf.placeholder(tf.float32, shape=[None, self.n_actions])
+
+            f1 = 1 / np.sqrt(self.fc1_dims)
+            dense1 = tf.keras.layers.Dense(
+                self.input, units=self.fc1_dims,
+                kernel_initializer=random_uniform(shape=(-f1, f1)),  # tensorflow v2 model
+                bias_initializer=random_uniform(shape=(-f1, f1))
+            )
+            batch1 = tf.keras.layers.BatchNormalization(dense1)
+            layer1_activation = tf.nn.relu(batch1)
+
+            f2 = 1 / np.sqrt(self.fc2_dims)
+            dense2 = tf.keras.layers.Dense(
+                self.input, units=self.fc2_dims,
+                kernel_initializer=random_uniform(shape=(-f2, f2)),  # tensorflow v2 model
+                bias_initializer=random_uniform(shape=(-f2, f2))
+            )
+            batch2 = tf.keras.layers.BatchNormalization(dense2)
+            layer2_activation = tf.nn.relu(batch2)
+
+            f3 = 0.003
+            mu = tf.keras.layers.Dense(layer2_activation, units=self.n_actions,
+                                       activation='tanh',
+                                       kernel_initializer=random_uniform(shape=(-f3, f3)),
+                                       bias_initializer=random_uniform(shape=(-f3, f3)))
+            self.mu = tf.multiply(mu, self.action_bound)
+
+    def predict(self, inputs):
+        return self.sess.run(self.mu, feed_dict={self.input: inputs})
+
+    def train(self, inputs, gradients):
+        self.sess.run(self.optimize, feed_dict={self.inputs: inputs, self.action_gradient: gradients})
+
+    # Continue 11/22/2021
