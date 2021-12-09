8e2024a0b7a1907664bfd139d6959b22e6560ddf
kc900201 <nkc900201@gmail.com>
2021-12-09 14:05:38 +0900

    Writing for pygame - punch the chimp (Done). Waiting for implementation

--- Projects/Pygame/PunchTheChimp/src.py
+++ Projects/Pygame/PunchTheChimp/src.py
@@ -58,32 +58,74 @@ class Fist(pg.sprite.Sprite):
 
     # Continue
     def update(self):
-        pass
+        """move the fist based on the mouse position"""
+        pos = pg.mouse.get_pos()
+        self.rect.topleft = pos
+        self.rect.move_ip(self.fist_offset)
+        if self.punching:
+            self.rect.move_ip(15, 25)
 
-    def punch(self):
-        pass
+    def punch(self, target):
+        """returns true if the fist collides with the target"""
+        if not self.punching:
+            self.punching = True
+            hitbox = self.rect.inflate(-5, -5)
+            return hitbox.colliderect(target.rect)
 
     def unpunch(self):
-        pass
+        """called to pull the fist back"""
+        self.punching = False
 
 
 class Chimp(pg.sprite.Sprite):
 
     def __init__(self):
-        pass
+        pg.sprite.Sprite.__init__(self)  # call Sprite initializer
+        self.image, self.rect = load_image("chimp.png", -1, 4)
+        screen = pg.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.topleft = 10, 90
+        self.move = 18
+        self.dizzy = False
 
     def update(self):
-        pass
+        """walk or spin, depending on the monkeys state"""
+        if self.dizzy:
+            self._spin()
+        else:
+            self._walk()
 
     def _walk(self):
-        pass
+        """move the monkey across the screen, and turn at the ends"""
+        newpos = self.rect.move((self.move, 0))
+        if not self.area.contains(newpos):
+            if self.rect.left < self.area.left or self.rect.right > self.area.right:
+                self.move = -self.move
+                newpos = self.rect.move((self.move, 0))
+                self.image = pg.transform.flip(self.image, True, False)
+        self.rect = newpos
 
     def _spin(self):
-        pass
+        """spin the monkey image"""
+        center = self.rect.center
+        self.dizzy = self.dizzy + 12
+        if self.dizzy >= 360:
+            self.dizzy = False
+            self.image = self.original
+        else:
+            rotate = pg.transform.rotate
+            self.image = rotate(self.original, self.dizzy)
+        self.rect = self.image.get_rect(center=center)
 
     def punched(self):
-        pass
+        """this will cause monkey to start spinning"""
+        if not self.dizzy:
+            self.dizzy = True
+            self.original = self.image
 
 
 if __name__ == '__main__':
     pg.init()
+    screen = pg.display.set_mode((1280, 480), pg.SCALED)
+    pg.display.set_caption("Monkey Fever")
+    pg.mouse.set_visible(False)
