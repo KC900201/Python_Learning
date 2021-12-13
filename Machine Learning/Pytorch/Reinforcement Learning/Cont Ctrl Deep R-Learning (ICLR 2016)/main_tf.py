1e9c7b0abd75c464f413fdc8254474eced66e6a4
kc900201 <nkc900201@gmail.com>
2021-12-13 13:38:50 +0900

    Continue developing code from real inforcement learning

---
+++ Machine Learning/Pytorch/Reinforcement Learning/Cont Ctrl Deep R-Learning (ICLR 2016)/main_tf.py
@@ -0,0 +1,7 @@
+# Youtube reference for paper implementation - https://www.youtube.com/watch?v=GJJc1t0rtSU&ab_channel=freeCodeCamp.org
+
+from ddpg_tf_org import Agent
+import gym
+import numpy as np
+
+from utils import plotLearning
